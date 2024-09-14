17bbf1a5f899dcf75345f4d5d34ad7a7d9e627ad
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-09-14 22:02:54 +0700

    Init python

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import nest_asyncio
+import uvicorn
+
+app = FastAPI()
+@app.get('/')
+async def main () : 
+    return {'message': 'Hello World'}
+
+async def deploy() : 
+    return {'message': 'Deployed'}
+
+nest_asyncio.apply()
+uvicorn.run(app, host='0.0.0.0')
